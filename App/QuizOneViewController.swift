ef8497ea3b2cea6e0b4fbec5311dd40375ae4b6d
gxby <ybaaag@hotmail.co.uk>
2015-05-17 18:14:49 +0100

    puzzles updated and quiz

--- App/QuizOneViewController.swift
+++ App/QuizOneViewController.swift
@@ -10,43 +10,8 @@ import UIKit
 
 class QuizOneViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
 
-    }
-    
-    @IBAction func showAlert(sender: AnyObject)
-    {
-       
-        var myAlert = UIAlertController(title: "Alert", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.Alert);
-        
-        
-        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default){(ACTION) in println("Ok button tapped");
-            
-            
-            
-        
-        
-    }
-    
-        myAlert.addAction(okAction);
 
-        
-        self.presentViewController(myAlert, animated: true, completion: nil);
-        
-        
 
-        
-    }
-    
-    
-    
-    
-    
 }
-
