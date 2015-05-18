aab5c62fce93ca119f002722ae6c93bb2d9e018c
gxby <ybaaag@hotmail.co.uk>
2015-05-18 12:13:22 +0100

    everything

--- App/QuizTwoViewController.swift
+++ App/QuizTwoViewController.swift
@@ -11,8 +11,54 @@ import UIKit
 class QuizTwoViewController: UIViewController {
     
     
+    @IBOutlet weak var ButtonSailsbury: UIButton!
+    
+    @IBOutlet weak var ButtonDereham: UIButton!
+    
+    @IBOutlet weak var ButtonWiltshire: UIButton!
+    
+    @IBOutlet weak var ButtonDorset: UIButton!
     
     
+    
+    
+    @IBOutlet weak var LabelEnd: UILabel!
+  
+    
+    
+    
+    func Hide(){
+        LabelEnd.hidden = true
+
+    
+        
+    }
+    func UnHide(){
+        LabelEnd.hidden = false
+    }
+    
+
+
+    
+    
+    
+    @IBAction func ButtonSailsburyAction(sender: AnyObject) {
+        UnHide()
+        
+        
+    }
+    
+    @IBOutlet weak var ButtonDerehamAction: UIButton! {
+        UnHide()
+    }
+    
+    @IBOutlet weak var ButtonWiltshireAction: UIButton! {
+        UnHide()
+    }
+    
+    @IBOutlet weak var ButtonDorsetAction: UIButton! {
+        UnHide()
+    }
     
     
 }
